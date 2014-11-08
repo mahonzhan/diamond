d167f19e815c1874d3fe21ad8e9b4b35c4b94ac3
mahongzhan <mahonzhan@sina.com>
2014-11-08 13:34:26 +0800

    mod run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-. ../../diamond_env/bin/activate
-pidfile="/usr/local/diamond/var/run/diamond.pid"
+. /usr/local/diamond_env/bin/activate
+dir="/usr/local/diamond"
+pidfile="$dir/var/run/diamond.pid"
 if [ -f $pidfile ];then
+  echo "pid exists, restart now"
   kill `cat $pidfile`
+else
+  echo "Starting ..."
 fi
-env PYTHONDONTWRITEBYTECODE=x python ../bin/diamond -c ../etc/diamond/diamond.conf -p $pidfile
+env PYTHONDONTWRITEBYTECODE=x python $dir/bin/diamond -c $dir/etc/diamond/diamond.conf -p $pidfile
